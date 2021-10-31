f9d05ed756d3f0af76780ba2b5942cb3d3e9c7d3
nidroy <nidroy@mail.ru>
2021-10-31 16:48:22 +0300

    trade items
    установка предметов торговли в зависимости от класса персонажа

--- Unknown World of Magic/Assets/Scripts/Trading.cs
+++ Unknown World of Magic/Assets/Scripts/Trading.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trading : MonoBehaviour
 {
     public Animator tradingWindowAnim; // анимация окна торговли
     public NPC npc; // неигровой персонаж
+    public Sprite[] itemsSoldSprite; // все предметы на продажу
+    public Image[] itemsSoldImage; // поле вывода предметов на продажу
 
     private void Update()
     {
         OpenTradingWindow();
     }
 
+    #region OpenClose
+    
     // открыть окно торговли
     public void OpenTradingWindow()
     {
@@ -28,6 +33,17 @@ public class Trading : MonoBehaviour
         {
             npc.SetTrades(false);
             tradingWindowAnim.SetBool("isOpen", false);
+        }
+    }
+
+    #endregion
+
+    // установить предметы на продажу в зависимости от класса персонажа
+    public void SetItemsSold(int numberInitialItem)
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            itemsSoldImage[i].sprite = itemsSoldSprite[i + numberInitialItem];
         }
     }
 }
