5cc9a2d40979df8790196284a10fac7e6d78d8e7
nidroy <nidroy@mail.ru>
2021-11-09 02:31:57 +0300

    creating a client and server
    создание клиента и сервера

---
+++ Unknown World of Magic/Unknown World of Magic server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unknown_World_of_Magic_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
